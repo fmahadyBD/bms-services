661b28625fd557988f041ce1c50367b613932245
fmahadyBD <fmahady01@gmail.com>
2026-03-08 09:27:39 +0600

    first commit

---
+++ src/main/java/com/fmahadybd/bms_services/BmsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.fmahadybd.bms_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fmahadybd/bms_services/BmsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmahadybd.bms_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
